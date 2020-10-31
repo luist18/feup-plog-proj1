1cebbc7ad3b70ee3aa0a503cc0f05d6b40ae9213
Fabio Moreira <moreira.fbio8@gmail.com>
2020-10-31 20:54:59 +0000

    Minor changes!

--- src/game.pl
+++ src/game.pl
@@ -6,20 +6,35 @@ gameLoop(Board, Player1, Player2) :-
     write(Player2), write(' turn\n'),
     playerTurn(UpdatedBoard, FinalBoard, Player2),
     displayBoard(FinalBoard).
-    /*Add call to gameLoop(FinalBoard, Player1, Player2)*/
+    /*Add call to gameLoop(FinalBoard, Player1, Player2)
+    It will run indefinitely as we have no set end condition yet.*/
+
 
 
 playerTurn(Board, UpdatedBoard, Player):-
     (Player=='White', write('WHITE CHOOSING TURN\n') ; Player=='Black', write('BLACK CHOOSING PLAY\n')),
     write('Choose element to move\n'),
+    /* Getting position of piece to move */
     manageRow(Old_Row),
     manageColumn(Old_Column),
-    replaceElement(Old_Row-1, Old_Column-1, empty, Board, SecondBoard),
+    Real_Old_Row is Old_Row-1, /*If row ==1, index in the list is 1-1=0 */
+    Real_Old_Column is Old_Column-1,
+
+    /* Extracting the element that is to be moved for analysis */
+    findnth(Real_Old_Row, Board, ExtractedRow),
+    findnth(Real_Old_Column, ExtractedRow, Element),
+    
+    /* Getting the final position */
     write('Moving element to final position.\n'),
     manageRow(New_Row),
     manageColumn(New_Column),
-    (Player=='White', replaceElement(New_Row-1, New_Column-1, white, SecondBoard, UpdatedBoard)) ; (Player=='Black', replaceElement(New_Row-1, New_Column-1, black, SecondBoard, UpdatedBoard)).
+    Real_New_Row is New_Row-1,
+    Real_New_Column is New_Column-1,
+    replaceElement(Real_Old_Row, Real_Old_Column, empty, Board, SecondBoard),
+    replaceElement(Real_New_Row, Real_New_Column, Element, SecondBoard, UpdatedBoard).
 
 
 
 
+checkIfPieceIsSelectable(Player, Element, X) :-
+    (Player =='White', (Element==white, X = 1 ; X = -1) ; Player=='Black', (Element==black, X = 1 ; X = -1)).
--- src/input.pl
+++ src/input.pl
@@ -8,10 +8,13 @@ readChar(Input) :-
 manageRow(Old_Row) :-
     write('Row?'),
     get_char(Row), get_char(_),
-    convert_char_to_int(Row, Old_Row).
+    convert_char_to_int(Row, Old_Row),
+    write(Old_Row),nl.
 
 manageColumn(Old_Column) :-
     write('Column?'),
     get_char(Column), get_char(_),
-    convert_char_to_int(Column, Old_Column).
+    convert_char_to_int(Column, Old_Column),
+    write(Old_Column),nl.
+
 
