2fdf9b2b5254ecddd10b05b146c9a2c3d5094921
Fabio Moreira <moreira.fbio8@gmail.com>
2020-11-30 02:01:31 +0000

    Added middle dragon spawn

--- src/dragons.pl
+++ src/dragons.pl
@@ -15,7 +15,8 @@ set_empty_caves(_, Board, Board).
 
 spawn_dragons(Board, Caves, PiecesCount, NewBoard, NewCaves, NewPieceCount) :-
   spawn_left_dragon(Board, Caves, PiecesCount, LeftBoard, LeftCaves, LeftPieceCount),
-  spawn_right_dragon(LeftBoard, LeftCaves, LeftPieceCount, NewBoard, NewCaves, NewPieceCount).
+  spawn_right_dragon(LeftBoard, LeftCaves, LeftPieceCount, RightBoard, RightCaves, RightPieceCount),
+  spawn_middle_dragon(RightBoard, RightCaves, RightPieceCount, NewBoard, NewCaves, NewPieceCount).
 
 spawn_left_dragon(Board, true-C2-C3, pieces_count(white-WhiteCount, black-BlackCount), NewBoard, NewCaves, NewPieceCount) :-
   state(Player, _, _, _),
@@ -60,3 +61,27 @@ spawn_right_dragon(Board, C1-C2-true, pieces_count(white-WhiteCount, black-Black
   ).
 
 spawn_right_dragon(Board, Caves, Pieces, Board, Caves, Pieces) :- !.
+
+spawn_middle_dragon(Board, C1-true-C3, pieces_count(white-WhiteCount, black-BlackCount), NewBoard, NewCaves, NewPieceCount) :-
+  state(Player, _, _, _),
+  get_element(3-4, Board, TopElement),
+  get_element(5-4, Board, BottomElement),
+  get_element(4-3, Board, LeftElement),
+  get_element(4-5, Board, RightElement),
+  piece(TopElement, _, Player, _),
+  piece(BottomElement, _, Player, _),
+  piece(LeftElement, _, Player, _),
+  piece(RightElement, _, Player, _),
+  piece(CaveElement, _, Player, 5),
+  matrix_replace(Board, 4-4, CaveElement, NewBoard),
+  NewCaves = C1-true-C3,
+  (
+      Player == white,
+      NewWhiteCount is WhiteCount + 1,
+      NewPieceCount = pieces_count(white-NewWhiteCount, black-BlackCount)
+      ;
+      NewBlackCount is BlackCount + 1,
+      NewPieceCount = pieces_count(white-WhiteCount, black-NewBlackCount)
+  ).
+
+spawn_middle_dragon(Board, Caves, Pieces, Board, Caves, Pieces) :- !.
